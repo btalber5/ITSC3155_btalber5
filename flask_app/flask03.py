36afa8ba97982ad47f9765b7c11caf44064971b6
Braxton Talbert <btalber5@uncc.edu>
2021-10-22 15:58:32 -0400

    flask app #3 - added execution path for POST request for new note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -4,6 +4,7 @@
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)     # create an app
 
@@ -16,22 +17,30 @@ def index():
     return render_template('index.html',user = a_user)
 @app.route('/notes')
 def get_notes():
+    a_user = {'firstname': 'Braxton ', 'lastname': 'Talbert', 'email': 'btalber5@uncc.edu'}
     notes = {1: {'title': 'First Note', 'text': 'This is my first note.', 'date': '10-18-21'},
              2: {'title':'Second Note', 'text': 'This is my second note.', 'date': '10-19-21'},
              3: {'title': 'Third Note', 'text': 'This is my third note.', 'date': '10-20-21'}}
-    return render_template('notes.html',notes=notes)
+    return render_template('notes.html',notes=notes, user = a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
+    a_user = {'firstname': 'Braxton ', 'lastname': 'Talbert', 'email': 'btalber5@uncc.edu'}
     notes = {1: {'title': 'First Note', 'text': 'This is my first note.', 'date': '10-18-21'},
              2: {'title': 'Second Note', 'text': 'This is my second note.', 'date': '10-19-21'},
              3: {'title': 'Third Note', 'text': 'This is my third note.', 'date': '10-20-21'}}
-    return render_template('note.html', note=notes[int(note_id)])
+    return render_template('note.html', note=notes[int(note_id)], user = a_user)
 
-@app.route('/notes/new')
+@app.route('/notes/new', methods = ['GET','POST'])
 def new_note():
+
     a_user = {'firstname': 'Braxton ', 'lastname': 'Talbert', 'email': 'btalber5@uncc.edu'}
-    return render_template('new.html', user = a_user)
+    print("request method is ", request.method)
+
+    if(request.method == "POST"):
+        return '<h1> POST method used for this request</h1>'
+    else:
+        return render_template('new.html', user = a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
